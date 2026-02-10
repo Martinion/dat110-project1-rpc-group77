bc3cd3f49b17ef02a406e13bef572105103859a8
LillyFro <184953@stud.hvl.no>
2026-02-10 21:26:29 +0100

    DisplayDevice funksjonell

--- src/main/java/no/hvl/dat110/system/display/DisplayDevice.java
+++ src/main/java/no/hvl/dat110/system/display/DisplayDevice.java
@@ -11,14 +11,16 @@ public class DisplayDevice {
 		
 		System.out.println("Display server starting ...");
 		
-		// TODO - START
-		// implement the operation of the display RPC server
-		// see how this is done for the sensor RPC server in SensorDevice
-				
+		RPCServer displayserver = new RPCServer(Common.DISPLAYPORT);
+		
+		DisplayImpl display = new DisplayImpl((byte)Common.READ_RPCID, displayserver );
+		
+		displayserver.run();
+		
+		displayserver.stop();
+	
 		if (true)
 			throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO - END
 		
 		System.out.println("Display server stopping ...");
 		
