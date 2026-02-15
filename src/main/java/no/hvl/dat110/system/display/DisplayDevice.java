b73b607713b231e529cd253978dc8c330b9c9ca7
Martin <pedersen.martin9@gmail.com>
2026-02-15 14:56:38 +0100

    Display fix
    Fikset bug med DisplayDevice. sendte inn READ_RPCID istedenfor WRITE_RPCID som gjorde at ingenting var registrert for WRITE_RPCID i services

--- src/main/java/no/hvl/dat110/system/display/DisplayDevice.java
+++ src/main/java/no/hvl/dat110/system/display/DisplayDevice.java
@@ -13,7 +13,7 @@ public class DisplayDevice {
 		
 		RPCServer displayserver = new RPCServer(Common.DISPLAYPORT);
 		
-		DisplayImpl display = new DisplayImpl((byte)Common.READ_RPCID, displayserver );
+		DisplayImpl display = new DisplayImpl((byte)Common.WRITE_RPCID, displayserver );
 		
 		displayserver.run();
 		
